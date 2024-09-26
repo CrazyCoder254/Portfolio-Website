e536c22c7bec8a63503006b388ba7479b41114fd
KenTech <kenkiragu18@gmail.com>
2024-09-26 09:53:51 +0300

    padding on top added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -26,55 +26,55 @@ const Navbar = () => {
 
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          <Image
-            src="/images/logo.png"  // Ensure that the image is in the public directory or use absolute URLs
-            alt="GitHub Icon"
-            width={120}
-            height={120}
-            style={{
-              display: 'block',
-              margin: '0 auto',
-              filter: 'brightness(2)',  // Increase brightness
-              padding: '5px',           // Padding for the background
-              borderRadius: '8px',      // Rounded corners
-            }}
-          />
-
-        </Link>
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
-        </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="flex container py-1 md:py-2 lg:py-4 flex-wrap items-center justify-between mx-auto px-4">
+      <Link
+        href={"/"}
+        className="text-2xl md:text-5xl text-white font-semibold"
+      >
+        <Image
+          src="/images/logo.png"
+          alt="GitHub Icon"
+          width={120}
+          height={120}
+          style={{
+            display: 'block',
+            margin: '0 auto',
+            filter: 'brightness(2)',
+            padding: '5px',
+            borderRadius: '8px',
+          }}
+        />
+      </Link>
+      <div className="mobile-menu block md:hidden">
+        {!navbarOpen ? (
+          <button
+            onClick={() => setNavbarOpen(true)}
+            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+          >
+            <Bars3Icon className="h-5 w-5" />
+          </button>
+        ) : (
+          <button
+            onClick={() => setNavbarOpen(false)}
+            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+          >
+            <XMarkIcon className="h-5 w-5" />
+          </button>
+        )}
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
+      <div className="menu hidden md:block md:w-auto" id="navbar">
+        <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLink href={link.path} title={link.title} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+  </nav>
+
   );
 };
 
